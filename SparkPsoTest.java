d0b956ff9d98e413b27dc5b40ab65ff1caea9bf3
King Can <king@hbut.edu.cn>
2019-06-07 19:06:36 +0800

    Update SparkPsoTest.java

--- SparkPsoTest.java
+++ SparkPsoTest.java
@@ -7,6 +7,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.storage.StorageLevel;
 import scala.Serializable;
 
 import java.util.Arrays;
@@ -17,37 +18,22 @@ public class SparkPsoTest extends PsoWhole implements Serializable {
 
     private static final long serialVersionUID = 6856557851928304976L;
 
-    public SparkPsoTest(){
-
-    }
-
-    public SparkPsoTest(int dim, int size, int iter, Range[] ranges, PsoUnit[] group, double[] positionBest, double valueBest, double xMax, double xMin, double maxV, double c1, double c2, double w, double[] wholeValue) {
-        super(dim, size, iter, ranges, group, positionBest, valueBest, xMax, xMin, maxV, c1, c2, w, wholeValue);
-    }
-
-    public SparkPsoTest(int dim, int size, int iter, double min, double max) {
-        super(dim, size, iter, min, max);
-    }
-
-    public SparkPsoTest(int dim, int size, int iter, Range[] ranges) {
-        super(dim, size, iter, ranges);
-    }
-
     public static void main(String[] args) {
         SparkConf sparkConf = new SparkConf()
                 .setAppName("SparkPsoTest")
                 .setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(sparkConf);//将PSO简化到这种易学习程度
+        sc.setLogLevel("ERROR");
         int dim=10;
-        int size = 200;
-        int iter =10;
+        int size = 60;
+        int iter =500;
         Range range=new  Range(-100, 100);
         PsoWhole pso  = new PsoWhole(dim, size, iter, range.getMin(), range.getMax());
         pso.init();
         //pso.valueBest=100.0;
         sc.broadcast(pso.positionBest);
         sc.broadcast(pso.valueBest);
-        System.out.println("初始速度"+Arrays.toString(pso.getGroup(0).getVelocity()));
+        System.out.println("初始速度"+ Arrays.toString(pso.getGroup(0).getVelocity()));
         System.out.println("初始位置"+Arrays.toString(pso.getGroup(0).getBestPosition()));
         //pso.setFunction(new FunctionImpl(dim,"f1"));
         JavaRDD<PsoUnit> psos = sc.parallelize(Arrays.asList(pso.group));
@@ -61,10 +47,17 @@ public class SparkPsoTest extends PsoWhole implements Serializable {
                 psoUnit.setValue(pso.function.function(psoUnit.getPosition()));//设置pBest（i）
                 return  psoUnit;
             }
-                             });
+        });
         List<PsoUnit> hrosinit = pso2.collect();
+        pso.group=hrosinit.toArray(new PsoUnit[hrosinit.size()]);
+        //min()
+        for (PsoUnit psoeach : pso.group) {
+            if (psoeach.getValue() >pso.getBestValue()) {
+                pso.setBestValue(psoeach.getValue());
+                pso.setBestPosition(psoeach.getPosition());
+            }
+        }
 
-       pso.group=hrosinit.toArray(new PsoUnit[hrosinit.size()]);
         int currentIter=0;
         //-----------------------------开始迭代--------------------------------------------
         while(currentIter<iter) {
@@ -75,10 +68,11 @@ public class SparkPsoTest extends PsoWhole implements Serializable {
 
                 @Override
                 public PsoUnit call(PsoUnit psoUnit) throws Exception {
-                    pso.updateVelocity(psoUnit);
-                    pso.updatePosition(psoUnit);
-                    //psoUnit.setValue(pso.function.function(psoUnit.getPosition()));
-                    return psoUnit;
+                   PsoUnit newPso = pso.updateVelocity(psoUnit);
+                    PsoUnit newPso2 =pso.updatePosition(newPso);
+
+
+                    return newPso2;
                 }
             });
             JavaRDD<PsoUnit> pso33 = pso3.map(new Function<PsoUnit, PsoUnit>() {
@@ -88,7 +82,7 @@ public class SparkPsoTest extends PsoWhole implements Serializable {
                 public PsoUnit call(PsoUnit psoUnit) throws Exception {
                     pso.setFunction(new FunctionImpl(dim,"f1"));
                     double value= pso.function.function(psoUnit.getPosition());
-                    if (value>pso.getBestValue()){
+                    if (value>psoUnit.getValue()){
                         psoUnit.setValue(value);
                         psoUnit.setBestPosition();
                     }
@@ -97,7 +91,7 @@ public class SparkPsoTest extends PsoWhole implements Serializable {
             });
 
             List<PsoUnit> display = pso33.collect();
-           // Collections.addAll(display, pso.group);
+            // Collections.addAll(display, pso.group);
             pso.group = display.toArray(new PsoUnit[display.size()]);//更新group
             for (PsoUnit psoeach : pso.group) {
                 if (psoeach.getValue() >pso.getBestValue()) {
@@ -106,15 +100,18 @@ public class SparkPsoTest extends PsoWhole implements Serializable {
                 }
             }
 
-            System.out.println(currentIter+"代:全局最优位置" + Arrays.toString(pso.getBestPosition()));
-            System.out.println(currentIter+":次迭代的最优值" + pso.getBestValue());
-            System.out.println(display.toArray());
-            for (PsoUnit ds : display) {
-                System.out.println("更新后的速度？" + Arrays.toString(ds.getVelocity()));
-                System.out.println("更新后的位置？" + Arrays.toString(ds.getBestPosition()));
-            }
+            //System.out.println(currentIter+"代:全局最优位置" + Arrays.toString(pso.getBestPosition()));
+            // System.out.println(currentIter+":次迭代的最优值" + pso.getBestValue());
+            //System.out.println(display.toArray());
+//            for (PsoUnit ds : display) {
+//                System.out.println("更新后的速度？" + Arrays.toString(ds.getVelocity()));
+//                System.out.println("更新后的位置？" + Arrays.toString(ds.getBestPosition()));
+//            }
+           // System.out.println(currentIter+":次迭代的最优值" + pso.getBestValue());
+            pso.setW((1.0d-(double)(currentIter%(iter/100))/(double)(iter/100)));
             currentIter++;
         }
-             sc.close();
+        System.out.println(currentIter+":最终迭代的最优值" + pso.getBestValue());
+        sc.close();
     }
 }
